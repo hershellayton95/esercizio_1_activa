3a893681053a9f6b7eefc133bfce39e553fd5046
Filippo Di Marco <filidm5795@gmail.com>
2023-07-12 16:06:43 +0200

    res:#00002 update route

--- src/routers/userRoute.js
+++ src/routers/userRoute.js
@@ -1,39 +1,24 @@
 import { Router } from "express";
+import {
+  createUser,
+  updateUser,
+  deleteUser,
+  findUserById,
+  getUsers,
+} from "../controllers/userController.js";
 
 const router = Router();
 
 // 1. creazione di un nuovo User;
-router.post("/", async (req, res) => {
-  res.json({
-    path: "creazione di un nuovo User",
-  });
-});
+router.post("/", createUser);
 // 2. aggiornamento di un utente tramite il suo id;
-router.put("/:id(\\d+)", async (req, res) => {
-  res.json({
-    path: "aggiornamento di un utente tramite il suo id",
-  });
-});
+router.put("/:id(\\d+)", updateUser);
 // 3. cancellazione di un utente tramite il suo id;
-router.delete("/:id(\\d+)", async (req, res) => {
-  res.json({
-    path: "cancellazione di un utente tramite il suo id",
-  });
-});
+router.delete("/:id(\\d+)", deleteUser);
 // 4. ricerca di un utente tramite id;
-router.get("/:id(\\d+)", async (req, res) => {
-  const userId = req.params.id;
-  res.json({
-    path: "ricerca di un utente tramite id " + userId,
-  });
-});
+router.get("/:id(\\d+)", findUserById);
 // 5. ricerca di tutti gli utenti
 // 6. ricerca di tutti gli utenti tramite luogo di nascita
-router.get("/", async (req, res) => {
-  const birthPlace = req.query.birthPlace;
-  res.json({
-    path: "ricerca di tutti gli utenti " + birthPlace,
-  });
-});
+router.get("/", getUsers);
 
 export default router;
