95d0ef5c915f3873bd237766b5e1803d817fe2ff
Filippo Di Marco <filidm5795@gmail.com>
2023-07-14 11:25:02 +0200

    ref: #0003 update mongoose

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import mongoose from "mongoose";
-import config from "./configurations/config.js";
+import config from "./configurations/variablesConfig.js";
 import app from "./app.js";
 
 const port = config.PORT;
@@ -9,11 +9,21 @@ const authName = config.MONGODB_USERNAME;
 const authPass = config.MONGODB_PASSWORD;
 const dbNameEnv = config.MONGODB_DB_NAME;
 
-mongoose
-  .connect(uri, { user: authName, pass: authPass, dbName: dbNameEnv })
-  .then(
-    app.listen(port, () => {
-      console.log(`[server]: Server is running at http://localhost:${port}`);
+const connectWithRetry = () => {
+  mongoose
+    .connect(uri, { user: authName, pass: authPass, dbName: dbNameEnv })
+    .then(() => {
+      console.log("Connected to MongoDB");
+      app.listen(port, () => {
+        console.log(`[server]: Server is running at http://localhost:${port}`);
+      });
     })
-  )
-  .catch((e) => console.log(e));
+    .catch((e) => {
+      console.error("Failed to connect to MongoDB:", e.message);
+      console.error("Server not started");
+      console.error("Retry Connection");
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
